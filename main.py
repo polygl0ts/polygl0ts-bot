ba1f04fa6882ccbaad9c5fb14f6df6917062b566
lromerio <lucio.romerio@epfl.ch>
2021-10-10 17:32:43 +0200

    Add verification code logic

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import discord
 import discord_slash
 from discord.ext import commands
-from discord_slash.utils.manage_commands import create_option, create_choice
-from discord_slash.model import SlashCommandOptionType
 from discord.utils import get
 
 import re
-import typing
 
 import config, captcha
 
 # Create bot
 intents = discord.Intents.default()
-#intents.members = True
-bot = discord.Client(intents=intents)
-slash = discord_slash.SlashCommand(bot, sync_commands=True)
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Connect bot
 @bot.event
@@ -27,12 +23,6 @@ async def on_member_join(member):
     print("someone joined!")
     await member.send("Hello there!")
 
-@slash.slash(name="ping", description="Just a test, sleeps for 5 seconds then replies with 'pong'", guild_ids=[config.user_verification.guild_id])
-async def ping(ctx: discord_slash.SlashContext):
-    await ctx.defer()
-    await asyncio.sleep(5)
-    await ctx.send("Pong!")
-
 # Helper to add member role
 async def add_member_role(bot, user_id):
     guild = bot.get_guild(config.user_verification.guild_id)
@@ -41,37 +31,19 @@ async def add_member_role(bot, user_id):
         [r for r in guild.roles if r.name == config.user_verification.role][0]
     )
 
-@slash.slash(name="send_code",
-             description="Send a verification code by email",
-             guild_ids=[config.user_verification.guild_id],
-             options=[
-                 create_option(name="email",
-                    description="Your EPFL email address",
-                    option_type=SlashCommandOptionType.STRING,
-                    required=True)
-                ]
-             )
-async def send_verification_code(ctx: discord_slash.SlashContext, email: typing.Optional[str] = None):
+@bot.command()
+async def email(ctx, email : str):
     r = r'[a-z]*.[a-z]*@epfl.ch'
     if re.match(r, email):
         code = captcha.generate_captcha(ctx.author.id)
-        await ctx.send(f'Here is your verifcation code: {code}')
+        await ctx.send(f'Here is your verification code: {code}')
     else:
         await ctx.send('This doesn\'t look like an EPFL email address to me...')
 
-@slash.slash(name="verify_code",
-             description="Verify code and assign member role",
-             guild_ids=[config.user_verification.guild_id],
-             options=[
-                 create_option(name="code",
-                    description="Your verification code",
-                    option_type=SlashCommandOptionType.STRING,
-                    required=True)
-                ]
-             )
-async def verify(ctx: discord_slash.SlashContext, code: typing.Optional[str] = None):
+@bot.command()
+async def verify(ctx, code: str):
     if captcha.validate_captcha(ctx.author.id, code):
-        await ctx.send("Such member, much wow!")
+        await ctx.send('Such member, much wow!')
         await add_member_role(ctx.bot, ctx.author.id)
     else:
         await ctx.send('Sorry, it looks like your code is invalid/expired.')
