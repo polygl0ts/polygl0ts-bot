47504297a173aa6d1708eb97f87242ccbf9837e6
Florian Hofhammer <florian.hofhammer@fhofhammer.de>
2021-10-10 16:38:40 +0200

    Reformat code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import re
 from discord.utils import get
+import re
 
 import config
 
@@ -9,36 +9,40 @@ import config
 # Create bot
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Connect bot
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print("We have logged in as {0.user}".format(bot))
+
 
 # When someone enters the discord, send him a message
 @bot.event
 async def on_member_join(member):
-    print('someone joined!')
-    await member.send('Hello there!')
+    print("someone joined!")
+    await member.send("Hello there!")
+
 
 # Helper to add member role
 async def add_member_role(bot, user_id):
     guild = bot.get_guild(config.user_verification.guild_id)
-    member = await(guild.fetch_member(user_id))
-    await member.add_roles([r for r in guild.roles if r.name == config.user_verification.role][0])
+    member = await (guild.fetch_member(user_id))
+    await member.add_roles(
+        [r for r in guild.roles if r.name == config.user_verification.role][0]
+    )
+
 
 # Verify command
 @bot.command()
-async def verify(ctx, checksum : str):
+async def verify(ctx, checksum: str):
     # if captcha.validate_answer(ctx.author.id, checksum):
     await ctx.send("That looks correct.\nCome on in!")
     await add_member_role(ctx.bot, ctx.author.id)
     # else:
     #     await ctx.send("That doesn't look correct.\n" + captcha.get_instructions(ctx.author.id))
 
+
 if __name__ == "__main__":
     # api.run_api(bot)
     bot.run(config.discord.token)
-
-
