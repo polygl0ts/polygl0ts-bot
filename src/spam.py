6526dae6896105ad281e5a610cc0da2c3459765e
Florian Hofhammer <florian.hofhammer@fhofhammer.de>
2023-11-08 18:58:41 +0100

    Switch from STARTTLS to full TLS for email

--- src/spam.py
+++ src/spam.py
@@ -1,9 +1,7 @@
+import config
 import smtplib
-import ssl
 
 from email.message import EmailMessage
-
-import config
 
 
 def send_mail(recipient, user_captcha):
@@ -14,12 +12,10 @@ def send_mail(recipient, user_captcha):
     msg["Subject"] = "Verification code for polygl0ts Discord server"
     msg.set_content(f'Hello {recipient}, your verification code is "{user_captcha}"!')
 
-    with smtplib.SMTP(
+    with smtplib.SMTP_SSL(
         host=config.email.smtp_host, port=config.email.smtp_port
     ) as smtp_conn:
         try:
-            # Want a secure connection - use STARTTLS
-            smtp_conn.starttls(context=ssl.create_default_context())
             # Mail go brrrrr
             smtp_conn.login(user=config.email.username, password=config.email.password)
             smtp_conn.sendmail(
